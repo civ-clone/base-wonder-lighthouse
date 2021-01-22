986c0526e1f81aca93a8f2890a7296cb57d3ae18
dom111 <dom111@users.noreply.github.com>
2021-01-22 20:42:47 +0000

    Use published package version for `core-wonder`

--- Lighthouse.d.ts
+++ Lighthouse.d.ts
@@ -1,4 +1,3 @@
 import { Wonder } from '@civ-clone/core-wonder/Wonder';
-export declare class Lighthouse extends Wonder {
-}
+export declare class Lighthouse extends Wonder {}
 export default Lighthouse;
